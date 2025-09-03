3610f231a556bb97ffbd0a266a563df180e25abe
Jorim13 <jorim.saltarin@gmail.com>
2025-09-03 16:17:23 -0500

    Primer commit del proyecto Rollerspeed

--- rollerspeed/src/main/java/com/rollerspeed/rollerspeed/RollerspeedApplication.java
+++ rollerspeed/src/main/java/com/rollerspeed/rollerspeed/RollerspeedApplication.java
@@ -8,6 +8,6 @@ public class RollerspeedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RollerspeedApplication.class, args);
+		System.out.println("Hello Spring Boot");
 	}
-
 }
