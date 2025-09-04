31f83d47e45eb8a9c0286d92fa03d3aabc7cff59
Jorim13 <jorim.saltarin@gmail.com>
2025-09-03 22:40:12 -0500

    Implementando PMV

--- rollerspeed/src/main/java/com/rollerspeed/rollerspeed/controllers/UserController.java
+++ rollerspeed/src/main/java/com/rollerspeed/rollerspeed/controllers/UserController.java
@@ -7,8 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class UserController {
-    @GetMapping("home")
-    public String Home() {
-        return "index";
+
+    // ---------------- HOME ----------------
+    @GetMapping({"", "home"}) // soporta "/" y "/home"
+    public String home() {
+        return "index"; // index.html en templates
+    }
+
+    // ---------------- CORPORATIVO ----------------
+
+    @GetMapping("corporativo/nosotros")
+    public String mision() {
+        return "corporativo/nosotros"; // mision.html en templates/corporativo
+    }
+
+    @GetMapping("corporativo/servicios")
+    public String servicios() {
+        return "corporativo/servicios"; // servicios.html en templates/corporativo
+    }
+
+    @GetMapping("corporativo/eventos")
+    public String eventos() {
+        return "corporativo/eventos"; // eventos.html en templates/corporativo
     }
 }
